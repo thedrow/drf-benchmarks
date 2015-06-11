f4c3f3808fc0e3b2ca39bb2a6731277f94a517c8
Omer Katz <omer.drow@gmail.com>
2015-06-11 22:36:05 +0300

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drf-benchmarks',
+    version='0.1.0',
+    packages=find_packages(),
+    url='',
+    license='BSD-3',
+    author='Omer Katz',
+    author_email='omer.drow@gmail.com',
+    description='Benchmarks suite for Django REST Framework'
+)
