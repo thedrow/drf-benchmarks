176eba3387341bc9c63908bcafdf4eb596dc4463
Omer Katz <omer.drow@gmail.com>
2015-08-17 18:50:59 +0300

    Added benchmarks that get the fields twice.

--- drf_benchmarks/serializers/test_model_serializer_get_fields.py
+++ drf_benchmarks/serializers/test_model_serializer_get_fields.py
@@ -7,7 +7,7 @@ import pytest
     group="ModelSerializer get_fields",
     warmup=True
 )
-def test_serializer_get_field(serializer, data, benchmark):
+def test_serializer_get_fields(serializer, data, benchmark):
     serializer = serializer(data=data)
 
     @benchmark
@@ -24,4 +24,30 @@ def test_nested_serializer_get_fields(nested_serializer, nested_data, benchmark)
 
     @benchmark
     def result():
+        return serializer.get_fields()
+
+
+@pytest.mark.benchmark(
+    group="ModelSerializer get_fields",
+    warmup=True
+)
+def test_serializer_get_fields_twice(serializer, data, benchmark):
+    serializer = serializer(data=data)
+
+    @benchmark
+    def result():
+        serializer.get_fields()
+        return serializer.get_fields()
+
+
+@pytest.mark.benchmark(
+    group="ModelSerializer get_fields",
+    warmup=True
+)
+def test_nested_serializer_get_fields_twice(nested_serializer, nested_data, benchmark):
+    serializer = nested_serializer(data=nested_data)
+
+    @benchmark
+    def result():
+        serializer.get_fields()
         return serializer.get_fields()
