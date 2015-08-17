b6fb957cad84f272713f3a2573e8d2f1da7cda60
Omer Katz <omer.drow@gmail.com>
2015-08-17 15:23:55 +0300

    Since the mixins clash create a new mixin with cythonized dicts and no copy.

--- drf_benchmarks/serializers/immutable_cython_ordered_dict/serializers.py
+++ drf_benchmarks/serializers/immutable_cython_ordered_dict/serializers.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from rest_framework import serializers
 
 from drf_benchmarks import test_serializer_fields, test_nested_serializer_fields
 from drf_benchmarks.models import RegularFieldsModel, RegularFieldsAndFKModel
-from drf_benchmarks.serializers.cython_ordered_dict import CythonOrderedDictMixin
-from drf_benchmarks.serializers.immutable.base import ImmutableModelSerializer
+from drf_benchmarks.serializers.immutable_cython_ordered_dict.base import ImmutableCythonOrderedDictMixin
 
 
-class TestSerializer(CythonOrderedDictMixin, ImmutableModelSerializer):
+class TestSerializer(ImmutableCythonOrderedDictMixin, serializers.ModelSerializer):
     class Meta:
         model = RegularFieldsModel
         fields = test_serializer_fields
 
 
-class TestNestedSerializer(CythonOrderedDictMixin, ImmutableModelSerializer):
+class TestNestedSerializer(ImmutableCythonOrderedDictMixin, serializers.ModelSerializer):
     fk = TestSerializer()
 
     class Meta:
