7ae38953a4a3ca6bb9b9201b7527f98cfaa9454a
Omer Katz <omer.drow@gmail.com>
2015-08-18 23:44:40 +0300

    Profile list serialization and deserialization and fixed wrong method calls in deserialization benchmarks.

--- drf_benchmarks/serializers/test_serializer_to_internal_value_benchmarks.py
+++ drf_benchmarks/serializers/test_serializer_to_internal_value_benchmarks.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from line_profiler import LineProfiler
+import os
 import pytest as pytest
 from rest_framework import serializers
+
+try:
+    from cProfile import Profile
+except ImportError:
+    from profile import Profile
+
+
+def profile_list_deserialization(serializer, child_serializer, data_list):
+    if os.environ.get('CI', None) != 'true':
+        return
+    profile = LineProfiler(serializer.to_internal_value, child_serializer.to_internal_value)
+    profile.enable()
+    serializer.to_internal_value(data_list)
+    profile.disable()
+    profile.print_stats()
 
 
 @pytest.mark.benchmark(
@@ -25,11 +42,14 @@ def test_object_deserialization(serializer_class, data, benchmark):
     warmup=True
 )
 def test_object_list_deserialization(serializer_class, data_list, benchmark):
-    serializer = serializers.ListSerializer(child=serializer_class(), data=data_list)
+    child_serializer = serializer_class()
+    serializer = serializers.ListSerializer(child=child_serializer, data=data_list)
 
     @benchmark
     def result():
         return serializer.to_internal_value(data_list)
+
+    profile_list_deserialization(serializer, child_serializer, data_list)
 
     assert result if data_list else not result and serializer.is_valid(), serializer.errors
 
@@ -44,7 +64,7 @@ def test_nested_object_deserialization(nested_serializer_class, nested_data, ben
 
     @benchmark
     def result():
-        return serializer.to_representation(nested_data)
+        return serializer.to_internal_value(nested_data)
 
     assert result and serializer.is_valid(), serializer.errors
 
@@ -55,10 +75,13 @@ def test_nested_object_deserialization(nested_serializer_class, nested_data, ben
 )
 @pytest.mark.django_db
 def test_nested_object_list_deserialization(nested_serializer_class, data_list_with_nesting, benchmark):
-    serializer = serializers.ListSerializer(child=nested_serializer_class(), data=data_list_with_nesting)
+    child_serializer = nested_serializer_class()
+    serializer = serializers.ListSerializer(child=child_serializer, data=data_list_with_nesting)
 
     @benchmark
     def result():
-        return serializer.to_representation(data_list_with_nesting)
+        return serializer.to_internal_value(data_list_with_nesting)
+
+    profile_list_deserialization(serializer, child_serializer, data_list_with_nesting)
 
     assert result if data_list_with_nesting else not result and serializer.is_valid(), serializer.errors
--- drf_benchmarks/serializers/test_serializer_to_representation_benchmarks.py
+++ drf_benchmarks/serializers/test_serializer_to_representation_benchmarks.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
+from line_profiler import LineProfiler
+import os
 
 import pytest as pytest
 from rest_framework import serializers
+
+
+def profile_list_serialization(serializer, child_serializer, instances_list):
+    if os.environ.get('CI', None) != 'true':
+        return
+    profile = LineProfiler(serializer.instances_list, child_serializer.instances_list)
+    profile.enable()
+    serializer.to_representation(instances_list)
+    profile.disable()
+    profile.print_stats()
 
 
 @pytest.mark.benchmark(
@@ -26,11 +38,14 @@ def test_object_serialization(instance, serializer_class, benchmark):
 )
 @pytest.mark.django_db
 def test_object_list_serialization(instances_list, serializer_class, benchmark):
-    serializer = serializers.ListSerializer(child=serializer_class(instance=instances_list))
+    child_serializer = serializer_class(instance=instances_list)
+    serializer = serializers.ListSerializer(child=child_serializer)
 
     @benchmark
     def result():
         return serializer.to_representation(instances_list)
+
+    profile_list_serialization(serializer, child_serializer, instances_list)
 
     assert result if instances_list else not result
 
@@ -56,10 +71,13 @@ def test_nested_object_serialization(nested_instance, nested_serializer_class, b
 )
 @pytest.mark.django_db
 def test_nested_object_list_serialization(nested_instances_list, nested_serializer_class, benchmark):
-    serializer = serializers.ListSerializer(child=nested_serializer_class(instance=nested_instances_list))
+    child_serializer = nested_serializer_class(instance=nested_instances_list)
+    serializer = serializers.ListSerializer(child=child_serializer)
 
     @benchmark
     def result():
         return serializer.to_representation(nested_instances_list)
+
+    profile_list_serialization(serializer, child_serializer, nested_instances_list)
 
     assert result if nested_instances_list else not result
